246915785eb470c1ce6cefa64762fb8033fd414a
Frank Carnovale <frank.carnovale@gmail.com>
2016-12-31 10:45:55 +0000

    clear postgres's password too

--- test_user.sql
+++ test_user.sql
@@ -1,5 +1,6 @@
 
-create database test_db;
+alter  user postgres  with password 'x';
 create user test_user with password 'x';
+create database test_db;
 grant all privileges on database test_db to test_user;
 
